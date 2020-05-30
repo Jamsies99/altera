d2594d4a85104261da2aee5e6c5500dfe2bf05f5
mrh544 <mrh544@nyu.edu>
2020-05-30 00:32:37 -0400

    Updates

---
+++ precache-manifest.b19d474e3480ece9b7540c5f97fdab87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b88a2fd5da7a0af9a24dc203832b66d",
+    "url": "/altera/index.html"
+  },
+  {
+    "revision": "a7da2e0d92c0d6b24b73",
+    "url": "/altera/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87b8a440a5bd975cf214",
+    "url": "/altera/static/js/2.a6313da8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/altera/static/js/2.a6313da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7da2e0d92c0d6b24b73",
+    "url": "/altera/static/js/main.098e4d95.chunk.js"
+  },
+  {
+    "revision": "6a2f76bbd572f4b65248",
+    "url": "/altera/static/js/runtime-main.b39a9f8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/altera/static/media/logo.5d5d9eef.svg"
+  }
+]);
